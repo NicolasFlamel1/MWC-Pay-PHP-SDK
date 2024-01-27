458b462ac5a9e943a61d30c6b05bde44728cd677
donovan6000 <nicolasflamel@mwcwallet.com>
2024-01-27 08:18:23 -0800

    Minimum needed to test releasing as package

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # MWC Pay PHP SDK
 
 ### Description
-PHP SDK to assist with using [MWC Pay](https://github.com/NicolasFlamel1/MWC-Pay).
+PHP SDK for [MWC Pay](https://github.com/NicolasFlamel1/MWC-Pay).
---
+++ src/MwcPay.php
@@ -0,0 +1,22 @@
+<?php
+
+
+// Namespace
+namespace Nicolasflamel\MwcPay;
+
+
+// Classes
+
+// MWC Pay class
+class MwcPay {
+
+	// Create payment
+	public static function createPayment() {
+	
+		// Do something
+		return "test";
+	}
+}
+
+
+?>
